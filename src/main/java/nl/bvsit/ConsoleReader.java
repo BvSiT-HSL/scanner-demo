05553bdcd6a376c3df2976174d9c0c5a8b4da312
BvSiT <34773187+BvSiT@users.noreply.github.com>
2022-02-09 12:36:01 +0100

    ConsoleReader tested

--- src/main/java/nl/bvsit/ConsoleReader.java
+++ src/main/java/nl/bvsit/ConsoleReader.java
@@ -3,27 +3,14 @@ package nl.bvsit;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+
+//https://stackoverflow.com/questions/61537142/how-to-loop-a-user-input-until-integer-is-entered
+//TODO Better use Reader directly in stead of Scanner?
+//TODO Rename since System.in does not read necessarily from console?
+
 public class ConsoleReader {
     private static String PROMPT="Type a whole number:";
     private static String PROMPT_BETWEEN="Type a whole number between %d and %d" ;
-
-/*    public static int read(){
-        Scanner scanner = new Scanner(System.in);
-        int x;
-        while (true){
-            try{
-                System.out.println(PROMPT);
-                x = scanner.nextInt();
-                break;
-            }
-            catch (InputMismatchException e){
-                scanner.next();//Move read position past delimiter after last read token
-            }
-        }
-        scanner.close();
-        return x;
-    }
-*/
 
     public static int read(){
         return read(null,null);
@@ -35,8 +22,9 @@ public class ConsoleReader {
         int x;
         while (true){
             try{
-
-                System.out.println(String.format(PROMPT_BETWEEN, min,max));
+                System.out.println( limitRange ? String.format(PROMPT_BETWEEN, min,max)
+                                        : String.format(PROMPT));
+                //TODO When string with spaces avoid multiple prompts
                 x = scanner.nextInt();
                 if (limitRange && (x<min || x > max) ) continue;
                 break;
@@ -48,6 +36,4 @@ public class ConsoleReader {
         scanner.close();
         return x;
     }
-
-
 }
--- src/main/java/nl/bvsit/Main.java
+++ src/main/java/nl/bvsit/Main.java
@@ -5,26 +5,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Scanner scanner = new Scanner(System.in);
-        Scanner scanner = new Scanner("a 7");
-        System.out.println("Type integer:");
-        //String input = scanner.next();
-
-        int x=0;
-        while (true){
-            try{
-                x = scanner.nextInt();
-                break;
-            }
-            catch (InputMismatchException e){
-                System.out.println("Enter a whole number!");
-                scanner.next();//Move read position past delimiter after last read token
-            }
-        }
-
-        System.out.println(x);
-        scanner.close();
-
-
+        //Read any integer
+        System.out.println(ConsoleReader.read());
+        //Read integer in range
+        System.out.println(ConsoleReader.read(1,5));
     }
 }
